3317cc6564c10c8690ef9b2d2a00c9f602fced2a
AhmedOT <iAhmedOT@gmail.com>
2020-07-01 16:22:13 -0700

    - Cleaned up the code and minimize it to be less than 25 lines.

--- SalesTax/ViewController.swift
+++ SalesTax/ViewController.swift
@@ -13,19 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var salesTaxTxt: UITextField!
     @IBOutlet weak var totalPriceLbl: UILabel!
     
-    @IBAction func calculateTotalPrice(_ sender: Any) {
-        let price = Double(priceTxt.text!)!
-        let salesTax = Double(salesTaxTxt.text!)!
-        
-        let totalSalesTax = price * salesTax
-        let totalPrice = price + totalSalesTax
-        totalPriceLbl.text = "$\(totalPrice)"
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    @IBAction func calculateTotalPrice(_ sender: Any) {
+        totalPriceLbl.text = "$\(Double(priceTxt.text!)! + (Double(priceTxt.text!)! * Double(salesTaxTxt.text!)!))"
+    }
 }
-
